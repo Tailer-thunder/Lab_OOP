cf69c3dd4387795a4047f50bc23bb5b2581df433
Tailer-thunder <korobejnikov.d@inbox.ru>
2024-09-11 22:00:42 +0300

    First lab

---
+++ Lab_1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include "include/closest_pair_tonum.h"
+
+int main(){
+    int upper_limit;
+    while (true)
+    {
+        if (!(std::cin >> upper_limit)) {
+            std::cin.clear();
+            std::cin.ignore();
+            std::cout << "Wrong input" << std::endl << std::endl;
+            continue;
+        }
+        if (upper_limit > 0){
+            break;
+        }else {
+            std::cin.clear();
+            std::cin.ignore();
+            std::cout << "The number must be greater than zero" << std::endl << std::endl;
+            continue;
+        }
+    }
+    std::cout << closest_pair_tonum(upper_limit);
+}
